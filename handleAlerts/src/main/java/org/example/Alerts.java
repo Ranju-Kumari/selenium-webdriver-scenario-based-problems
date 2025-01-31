e599bb11f6c2b00464f6fdc61e684bb46f892679
Ranju Kumari <rjsky009@gmail.com>
2025-01-31 22:55:59 +0530

    Updated folder structure

--- handleAlerts/src/main/java/org/example/Alerts.java
+++ handleAlerts/src/main/java/org/example/Alerts.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Main {
+public class Alerts {
     public static void main(String[] args) throws InterruptedException {
 
         WebDriverManager.chromedriver().setup();
